6061787481d465d05f28fb8f794346dea2ba2c8b
Le Botlan <contact.lebotlan@insa-toulouse.fr>
2017-07-08 22:14:51 +0200

    Lwt

--- src/lwt_tester.ml
+++ src/lwt_tester.ml
@@ -1,4 +1,4 @@
 open Exenum_internals
        
-let lwt_tester exen ?from ?upto ?verbose_period ?tos ~len f =
+let tester exen ?from ?upto ?verbose_period ?tos ~len f =
  Tester.gen_tester Lwt_io.print ?tos Lwt.bind Lwt.return_unit (Exenum.get_exen exen) ?from ?upto ?verbose_period ~len f
